aea41fcc513c26fca9167fedd1a757e021968032
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2016-07-13 02:43:45 +0200

    Improve eq rate computation

--- app/models/interest_rate.rb
+++ app/models/interest_rate.rb
@@ -16,17 +16,11 @@
 class InterestRate < ActiveRecord::Base
   belongs_to :object, polymorphic: true
 
-  def self.equivalent_rate(transactions, current_value, lower_bound, upper_bound, max_iter = 30)
+  def self.equivalent_rate(transactions, current_value, lower_bound, upper_bound, max_iter = 300)
     raise 'Maximum iterations reached' if max_iter == 0
 
     candidate = (lower_bound + upper_bound) / 2.0
-
-    total = 0
-    transactions.each do |t|
-      days = Date.today - t.done_at
-      total += t.amount_original * (1 + candidate)**(days / 365.0)
-    end
-
+    total = compute_interest(transactions, candidate)
     delta = total - current_value
 
     return candidate if delta < 0.001 && delta > - 0.001
@@ -36,5 +30,44 @@ class InterestRate < ActiveRecord::Base
     else
       return equivalent_rate(transactions, current_value, lower_bound, candidate, max_iter - 1)
     end
+  end
+
+  def self.compute_interest(transactions, rate)
+    amount_by_day = transactions.group_by(&:done_at).map { |date, ts| { date: date, value: ts.map(&:amount_original).reduce(:+) } }
+    amount_by_day = amount_by_day.sort_by { |h| h[:date] }
+
+    running_total = 0
+    dates_per_amount = []
+    (1..amount_by_day.length).each do |i|
+      from = amount_by_day[i - 1][:date]
+      to = (i == amount_by_day.length) ? Date.today : amount_by_day[i][:date]
+      amount = running_total + amount_by_day[i - 1][:value]
+      running_total += amount_by_day[i - 1][:value]
+
+      dates_per_amount << { from: from, to: to, amount: amount }
+    end
+
+    days_per_amount = []
+    dates_per_amount.each do |h|
+      from = h[:from]
+      to = h[:to]
+
+      (from.year..to.year).each do |y|
+        from = (h[:from].year == y) ? h[:from] : from.beginning_of_year
+        to = (h[:to].year == y) ? h[:to] : from.end_of_year + 1
+        year_length = 1 + (Date.new(y, 1, 1).end_of_year - Date.new(y, 1, 1))
+
+        days_per_amount << { days: to - from, amount: h[:amount], year_length: year_length }
+
+        from = to
+      end
+    end
+
+    total_pv = 0
+    days_per_amount.each do |h|
+      total_pv += h[:amount].to_f * ((1 + rate.to_f)**(h[:days].to_f / h[:year_length].to_f) - 1)
+    end
+
+    total_pv + days_per_amount.last[:amount]
   end
 end
