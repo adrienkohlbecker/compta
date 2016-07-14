7c953082c60da80bdada3111cab8aba2637bf419
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2016-07-14 03:25:14 +0200

    Fix

--- app/models/matview/portfolio_performance.rb
+++ app/models/matview/portfolio_performance.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 class Matview::PortfolioPerformance < Matview::Base
   def invested
-    @_invested ||= self[:invested].nil? ? nil : Amount.new(self[:invested], 'EUR', date)
+    @_invested ||= self[:invested_original].nil? ? nil : Amount.new(self[:invested_original], self[:invested_currency], self[:invested_date])
   end
 
   def current_value
-    @_current_value ||= self[:current_value].nil? ? nil : Amount.new(self[:current_value], 'EUR', date)
+    @_current_value ||= self[:current_value_original].nil? ? nil : Amount.new(self[:current_value_original], self[:current_value_currency], self[:current_value_date])
   end
 
   def pv
-    @_pv ||= self[:pv].nil? ? nil : Amount.new(self[:pv], 'EUR', date)
+    @_pv ||= self[:pv_original].nil? ? nil : Amount.new(self[:pv_original], self[:pv_currency], self[:pv_date])
   end
 end
