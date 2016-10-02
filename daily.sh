bf00b897eeed51ba231b3a8d6a7c44b96c2d4c11
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2016-10-02 19:38:09 +0200

    Fix paths

--- daily.sh
+++ daily.sh
@@ -11,8 +11,8 @@ function finish {
   open -a Dropbox
 
   echo "Stopping database..."
-  /usr/local/bin/docker-compose --file=/Users/adrien/Desktop/compta/backend/docker-compose.yml stop db
-  /usr/local/bin/docker-compose --file=/Users/adrien/Desktop/compta/backend/docker-compose.yml stop redis
+  /usr/local/bin/docker-compose --file=/Users/adrien/Desktop/compta/docker-compose.yml stop db
+  /usr/local/bin/docker-compose --file=/Users/adrien/Desktop/compta/docker-compose.yml stop redis
 
 }
 
@@ -26,17 +26,17 @@ echo "stopping Dropbox"
 killall Dropbox
 
 echo "Starting database..."
-/usr/local/bin/docker-compose --file=/Users/adrien/Desktop/compta/backend/docker-compose.yml create db
-/usr/local/bin/docker-compose --file=/Users/adrien/Desktop/compta/backend/docker-compose.yml start db
-/usr/local/bin/docker-compose --file=/Users/adrien/Desktop/compta/backend/docker-compose.yml create redis
-/usr/local/bin/docker-compose --file=/Users/adrien/Desktop/compta/backend/docker-compose.yml start redis
+/usr/local/bin/docker-compose --file=/Users/adrien/Desktop/compta/docker-compose.yml create db
+/usr/local/bin/docker-compose --file=/Users/adrien/Desktop/compta/docker-compose.yml start db
+/usr/local/bin/docker-compose --file=/Users/adrien/Desktop/compta/docker-compose.yml create redis
+/usr/local/bin/docker-compose --file=/Users/adrien/Desktop/compta/docker-compose.yml start redis
 sleep 20
 
 echo "Running daily task..."
-/usr/local/bin/docker-compose --file=/Users/adrien/Desktop/compta/backend/docker-compose.yml run --rm main rake daily
+/usr/local/bin/docker-compose --file=/Users/adrien/Desktop/compta/docker-compose.yml run --rm main rake daily
 
 echo "Backing up raw data..."
-/usr/local/bin/docker-compose --file=/Users/adrien/Desktop/compta/backend/docker-compose.yml run --rm pg_dump --format=p --verbose --encoding=UTF-8 --no-owner --no-privileges --inserts --dbname=compta_development --file="/dropbox/backups/$(date +%d).sql"
+/usr/local/bin/docker-compose --file=/Users/adrien/Desktop/compta/docker-compose.yml run --rm pg_dump --format=p --verbose --encoding=UTF-8 --no-owner --no-privileges --inserts --dbname=compta_development --file="/dropbox/backups/$(date +%d).sql"
 
 
 echo "Notify Deadmansnitch..."
