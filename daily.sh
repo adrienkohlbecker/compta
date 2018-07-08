917555ae4780e86103f168144267389e06980b23
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2018-07-08 23:42:17 +0100

    Paths

--- daily.sh
+++ daily.sh
@@ -8,8 +8,8 @@ IFS=$'\n\t'
 function finish {
 
   echo "Stopping database..."
-  /usr/local/bin/docker-compose --file=/Users/ak/Desktop/compta/docker-compose.yml stop db
-  /usr/local/bin/docker-compose --file=/Users/ak/Desktop/compta/docker-compose.yml stop redis
+  /usr/local/bin/docker-compose --file=/Users/ak/Work/compta/docker-compose.yml stop db
+  /usr/local/bin/docker-compose --file=/Users/ak/Work/compta/docker-compose.yml stop redis
 
 }
 
@@ -20,15 +20,15 @@ date
 echo ""
 
 echo "Starting database..."
-/usr/local/bin/docker-compose --file=/Users/ak/Desktop/compta/docker-compose.yml up -d db
-/usr/local/bin/docker-compose --file=/Users/ak/Desktop/compta/docker-compose.yml up -d redis
+/usr/local/bin/docker-compose --file=/Users/ak/Work/compta/docker-compose.yml up -d db
+/usr/local/bin/docker-compose --file=/Users/ak/Work/compta/docker-compose.yml up -d redis
 sleep 20
 
 echo "Running daily task..."
-/usr/local/bin/docker-compose --file=/Users/ak/Desktop/compta/docker-compose.yml run --rm main rake daily
+/usr/local/bin/docker-compose --file=/Users/ak/Work/compta/docker-compose.yml run --rm main rake daily
 
 echo "Backing up raw data..."
-/usr/local/bin/docker-compose --file=/Users/ak/Desktop/compta/docker-compose.yml run --rm pg_dump --format=p --verbose --encoding=UTF-8 --no-owner --no-privileges --inserts --dbname=compta_development --file="/dropbox/backups/$(date +%d).sql"
+/usr/local/bin/docker-compose --file=/Users/ak/Work/compta/docker-compose.yml run --rm pg_dump --format=p --verbose --encoding=UTF-8 --no-owner --no-privileges --inserts --dbname=compta_development --file="/dropbox/backups/$(date +%d).sql"
 
 
 echo "Notify Deadmansnitch..."
