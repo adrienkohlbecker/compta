96a6033cdec187d979375d4fb9c35218957a020c
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2018-08-28 00:07:25 +0100

    Better log

--- lib/globals.rb
+++ lib/globals.rb
@@ -3,26 +3,28 @@ def refresh_quotations!
   [Currency, OpcvmFund].map do |model|
     query = (model == OpcvmFund) ? model.where(closed: false) : model
     query.all.reverse.map do |item|
-      ap item.name
+      puts "refresh #{item.name}"
       item.refresh_data
       item.refresh_quotation_history
     end
   end
+  puts 'refresh matview'
   Matview::Base.refresh_all
+  puts 'gnucash prices'
   GnuCash.refresh_from_quotations
 end
 
 def excel_export!(path)
   Portfolio.all.map do |portfolio|
-    ap portfolio.name
+    puts "excel: #{portfolio.name}"
     PortfolioFormatter.new(portfolio).excel("#{path}/#{portfolio.name}.xlsx")
   end
-  puts "Global"
+  puts 'excel: global'
   PortfolioFormatter.new(Portfolio.all.pluck(:id)).excel("#{path}/Global.xlsx")
-  puts "Currencies"
+  puts 'excel: Currencies'
   CommodityFormatter.new(Currency).excel("#{path}/Currencies.xlsx")
-  puts "OPCVM"
+  puts 'excel: OPCVM'
   CommodityFormatter.new(OpcvmFund).excel("#{path}/OPCVM.xlsx")
-  puts "SCPI"
+  puts 'excel: SCPI'
   CommodityFormatter.new(ScpiFund).excel("#{path}/SCPI.xlsx")
 end
